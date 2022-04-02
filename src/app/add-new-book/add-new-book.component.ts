ff1c0a890c7a08ab5b3944e764320ff61cda4d44
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-02 21:57:47 +0600

    updated update-book component

--- src/app/add-new-book/add-new-book.component.ts
+++ src/app/add-new-book/add-new-book.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+// import { ServerModule } from '@angular/platform-server';
 import { Router } from '@angular/router';
 import { book } from '../book';
 import { ServiceNameService } from '../service-name.service';
